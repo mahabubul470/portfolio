610b19fb4ce6818a442b206a564ac8cdd608b987
mahabubul470 <mahabubul470@gmail.com>
2023-10-26 20:31:37 +0000

    Deploying to gh-pages from @ mahabubul470/mahabubul470.github.io@14262e82af9a23f3b171721cc09a517fe3751199 🚀

---
+++ _app/immutable/chunks/paths.c1955edb.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_3xxiyj)==null?void 0:s.base)??"/mahabubul470.github.io";var a;const e=((a=globalThis.__sveltekit_3xxiyj)==null?void 0:a.assets)??t;export{e as a,t as b};
